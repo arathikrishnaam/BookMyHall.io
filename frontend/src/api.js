4ecbce53f0adf74de3cb1561f9afeb5645a5b403
ssreelakshmi04 <lakshmisreesuresh04@gmail.com>
2025-08-07 07:39:29 +0530

    updation

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL for your backend API
-const API_BASE_URL = 'https://bookmyhall-io.onrender.com'; // Assuming your Express routes are mounted under /api
+const API_BASE_URL = 'https://bookmyhall-io.onrender.com/api'; // Assuming your Express routes are mounted under /api
 
 // Helper function to get auth headers
 const getAuthHeaders = () => {
